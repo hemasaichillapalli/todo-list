cb40312c3fdf17768f231d26dd6d99e863d8b904
hemasai <hemasaichillapalli@gmail.com>
2026-06-15 00:00:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
